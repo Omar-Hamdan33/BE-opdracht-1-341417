dcce2c95a2c736e5feb5a4cf552b3c9db3515a0f
Omar-Hamdan33 <oh073827@gmail.com>
2025-11-02 20:10:04 +0100

    de view aangepast zodat die meer op de wire frame lijkt

--- resources/views/warehouse/index.blade.php
+++ resources/views/warehouse/index.blade.php
@@ -3,36 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title }}</title>
+    <title>Overzicht Magazijn Jamin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href=".../css/style.css">
 </head>
 <body>
-    <h1>{{ $title }}</h1>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Barcode</th>
-                <th>Naam</th>
-                <th>VerpakkingsEenheid</th>
-                <th>Location</th>
-                <th>Leverantie info</th>
-                <th>Alergeen info</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($data as $item)
+    <div class="container mt-4">
+        <h1>Overzicht Magazijn Jamin</h1>
+        <table class="table table-bordered">
+            <thead>
                 <tr>
-                    <td>{{ $item->Barcode }}</td>
-                    <td>{{ $item->Naam }}</td>
-                    <td>{{ $item->VerpakkingsEenheid }}</td>
-                    <td>{{ $item->AantalAanwezig }}</td>
-                    <td><a href="/leverancier/{{ $item->Id }}" class="bi bi-question-lg"></a></td>
-                    <td><a href="/alergeen/{{ $item->Id }}" class="bi bi-x-lg"></a></td>
+                    <th>Barcode</th>
+                    <th>Naam</th>
+                    <th>VerpakkingsEenheid</th>
+                    <th>AantalAanwezig</th>
+                    <th>Allergenen Info</th>
+                    <th>Leverantie Info</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach($data as $item)
+                    <tr>
+                        <td>{{ $item->Barcode }}</td>
+                        <td>{{ $item->Naam }}</td>
+                        <td>{{ $item->VerpakkingsEenheid }}</td>
+                        <td>{{ $item->AantalAanwezig }}</td>
+                        <td class="text-center">
+                            <a href="/alergeen/{{ $item->Id }}" class="bi bi-x-lg text-danger" style="font-size: 1.2em; text-decoration: none;"></a>
+                        </td>
+                        <td class="text-center">
+                            <a href="/leverancier/{{ $item->Id }}" class="bi bi-question-lg text-primary" style="font-size: 1.2em; text-decoration: none;"></a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
