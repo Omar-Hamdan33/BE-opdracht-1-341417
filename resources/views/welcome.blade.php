a0622cab8ac9dc9b428620f937bfa9fa938b5b69
Omar-Hamdan33 <oh073827@gmail.com>
2025-11-02 20:33:14 +0100

    Voeg mappen toe voor opdracht structuur (docs, vids, db)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,27 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jamin Magazijn Systeem</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
 </head>
 <body>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8 text-center">
-                <h1 class="mb-4">Welkom bij Jamin Magazijn Systeem</h1>
-                <p class="lead mb-4">Beheer uw magazijn en bekijk product informatie</p>
-                
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Magazijn Overzicht</h5>
-                        <p class="card-text">Bekijk alle producten in het magazijn, inclusief voorraad, allergenen en leverancier informatie.</p>
-                        <a href="/warehouse" class="btn btn-primary btn-lg">
-                            Ga naar Overzicht Magazijn Jamin
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 </body>
 </html>
